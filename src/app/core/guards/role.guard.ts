d60e3a8e332b630243f443f6613ede08fda5e1d0
refka <refka.fouzai@gmail.com>
2024-09-16 16:27:31 +0100

    set async access for currentuser and set styles for button

--- src/app/core/guards/role.guard.ts
+++ src/app/core/guards/role.guard.ts
@@ -1,55 +1,56 @@
 import { Injectable } from '@angular/core';
 import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
-
-
-import { Store } from '@ngrx/store';
-import { getUser } from 'src/app/store/Authentication/authentication-selector';
-import { AuthfakeauthenticationService } from '../services/authfake.service';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { _User } from 'src/app/store/Authentication/auth.models';
 
 @Injectable({ providedIn: 'root' })
 export class RoleGuard implements CanActivate {
     claims: any[] = [];
-   
+    private currentUserSubject: BehaviorSubject<_User>;
+    public currentUser: Observable<_User>;
     constructor(
         private router: Router,
-        private authFackservice: AuthfakeauthenticationService
-    ) { }
+       
+    ) { 
+      this.currentUserSubject = new BehaviorSubject<_User>(JSON.parse(localStorage.getItem('currentUser')));
+      this.currentUser = this.currentUserSubject.asObservable();
+    }
 
-    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    canActivate(route: ActivatedRouteSnapshot) {
  
         console.log('in claim guard');
-        const currentUser = this.authFackservice.currentUserValue;
-        if (currentUser) {
-          this.claims = currentUser.role.claims;
-          const requiredClaim = route.data?.['claim'];
-          console.log('***********************');
-          console.log(this.claims);
-          console.log(requiredClaim);
-          console.log('***********************');
-          if (!requiredClaim) {
-            return true; // no permission required, allow access
-          }
-       
-          const hasRequiredClaims = requiredClaim.some(requiredClaim => {
-            return this.claims.some(claim => {
-              return claim.type === requiredClaim.claimType && requiredClaim.claimValue.every(value => claim.value.includes(value));
-            });
+        this.currentUser.subscribe(user => {
+          if (user) {
+               
+                this.claims = user.role.claims;
+              }
           });
-          if (hasRequiredClaims) {
+          const requiredClaim = route.data?.['claim'];
+                console.log('***********************');
+                console.log(this.claims);
+                console.log(requiredClaim);
+                console.log('***********************');
+          if (!requiredClaim) {
+                  return true; // no permission required, allow access
+                }
+            
+         const hasRequiredClaims = requiredClaim.some(requiredClaim => {
+             return this.claims.some(claim => {
+             return claim.type === requiredClaim.claimType && requiredClaim.claimValue.every(value => claim.value.includes(value));
+          });
+           });
+         if (hasRequiredClaims) {
             return true; // user has all required permissions, allow access
           }
           else
-          {
-            this.router.navigate(['/pages/403']);
-            return false; // user doesn't have required permissions, deny access
-          }
+         {
+          this.router.navigate(['/pages/403']);
+          return false; // user doesn't have required permissions, deny access
+        }
 
         
         
         }
     
-    
-        
-    
-    }
+ 
 }
