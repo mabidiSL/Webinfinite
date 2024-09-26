0e140811b31d3e44dd3b1b4c5f6e8f637adb3d5a
refka <refka.fouzai@gmail.com>
2024-09-26 18:37:00 +0100

    set up phone

--- src/app/shared/widget/phone-number/phone-number.component.ts
+++ src/app/shared/widget/phone-number/phone-number.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output, SimpleChanges } from '@angular/core';
 import intlTelInput from 'intl-tel-input';
 
 @Component({
@@ -8,23 +8,42 @@ import intlTelInput from 'intl-tel-input';
 })
 export class PhoneNumberComponent {
   
+  
+  @Input() phone: string | undefined; // Add Input decorator to accept phone number from the parent
   @Output() phoneNumberChanged = new EventEmitter<string>();
+  iti: any;
+  
+  
   ngAfterViewInit() {
- //const input = this.phoneInput.nativeElement;
- setTimeout(() => {
+  
+    setTimeout(() => {
 
     const input = document.querySelector('#phoneInput') as HTMLInputElement;
-    intlTelInput(input, {
+    this.iti = intlTelInput(input, {
       initialCountry: 'sa', // you can change the initial country
       //preferredCountries: ['us', 'gb', 'fr'] as any, // add preferred countries
       utilsScript: 'node_modules/intl-tel-input/build/js/utils.js' // for validation and formatting
     });
+    
+    if(this.phone){
+      this.iti.setNumber(this.phone);
+     }
+
     input.addEventListener('input', () => {
-      const phoneNumber = input.value;
+      const phoneNumber = this.iti.getNumber();
       this.phoneNumberChanged.emit(phoneNumber);
     });
+
+    
   }, 100);
 
+   
   }
-  
+  ngOnChanges(changes: SimpleChanges) {
+    // Update the phone number when the input value changes
+    if (changes.phone && this.iti) {
+      
+      this.iti.setNumber(this.phone || '');
+    }
+  }
 }
